5f2b454ce94121e188e1adfb2efd6050bbb96512
Vincent <vlarochelle970@gmail.com>
2018-10-23 10:45:27 -0400

    Creation de la classe Jeton Positif

---
+++ jeu/jeu/JetonPositif.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jeu
+{
+    class JetonPositif:Jeton
+    {
+        JetonPositif()
+        {
+            GenererValeur();
+        }
+
+        public override void GenererValeur()
+        {
+
+            int valeurJeton = genrateurJeton.Next(5) + 50;
+        }
+    }
+}
